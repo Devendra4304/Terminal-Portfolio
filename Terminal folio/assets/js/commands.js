4eb03009c283a813451993a43aa8944597e15f38
singh devendra <141473323+Devendra4304@users.noreply.github.com>
2024-08-13 12:46:17 +0530

    Add files via upload

---
+++ Terminal folio/assets/js/commands.js
@@ -0,0 +1,42 @@
+const commands = {
+  "whoiam":{
+    "description":"Tell you who i am",
+    "multiline":false,
+    "content":"Hello! I'm Singh Devendra and I'm a software engineer. I like web development."
+  },
+  "web":{
+    "description":"Get my web page",
+    "multiline":false,
+    "content":"You can see my personal website at the following link https://devendra4304.github.io/portfolio/"
+  },
+  "github":{
+    "description":"Get my github profile",
+    "multiline":false,
+    "content":"I have a github profile which you can see at the link https://github.com/Devendra4304"
+  },
+  "education":{
+    "description":"Get my education timeline",
+    "multiline":true,
+    "content":[
+      "Place: University Of Engineering and Management, Jaipur  2021-Now  -  Degree: Computer Science Engineering",
+      "Place: Shree Swaminarayan Gurukul, Bhilad  2019-2020  -  School- Senior Secondary"
+    ]
+  },
+  "experience":{
+    "description":"Get my Internship timeline",
+    "multiline":true,
+    "content":[
+      "Cybersecurity Virtual Internship in AICTE PaloAlto Sep 2023 - Nov 2023",
+      "Web developer Virtual Internship in Code clause Feb 2024 - Mar 2024"
+    ]
+  },
+  "projects":{
+    "description":"Get cool projects I created.",
+    "multiline":true,
+    "content":[
+      "Portfolio  - Developed a personal Portfolio Website showcasing projects and skills,in HTML, CSS, Javascript.",
+      "E-voting System Dapp  -  Developed a E voting system where we can cast vote. Technologies used Truffle, Ganache, React",
+      "House Price Prediction  -  created a House Price Prediction where we can give user input and model will predict the price according to the input",
+    ]
+  }
+}
